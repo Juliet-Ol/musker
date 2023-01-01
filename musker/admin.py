d3c8f691f7a4e8b9192536c6fcb5919e5776d075
Juliet <agutu@gmail.com>
2023-01-02 01:55:20 +0300

    create a superuser

---
+++ musker/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+
+#unregister Groups
+admin.site.unregister(Group)
+
+#Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model= User
+    # Just display username field on admin page
+    fields = ["username"]
+
+#unregister initial User
+admin.site.unregister(User)
+#Reregister User
+admin.site.register(User, UserAdmin)
+
+
