2f33b1e179253721463419edcf83d330240a4e01
Juliet <agutu@gmail.com>
2023-01-04 16:54:30 +0300

    register profile on admin site

--- musker/admin.py
+++ musker/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-
+from .models import Profile
 #unregister Groups
 admin.site.unregister(Group)
 
@@ -13,7 +13,7 @@ class UserAdmin(admin.ModelAdmin):
 #Unregister initial User
 admin.site.unregister(User)
 
-#Reregister User
+#Reregister User and Profile
 admin.site.register(User, UserAdmin)
-
+admin.site.register(Profile)
 
