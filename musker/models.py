a5c8dec095346e5e32afb8dcf25ad2da47c00ea1
Juliet <agutu@gmail.com>
2023-01-04 18:31:26 +0300

    import sigals and create a function for profiles to automatically create and user to follow themselves

--- musker/models.py
+++ musker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 #create a user profile model
@@ -11,6 +12,18 @@ class Profile(models.Model):
         blank=True)
 
     def __str__(self):
-        return self.user.username    
+        return self.user.username  
+
+#Create Profile when new user signs up  
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        #Have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)        
+
 
 
